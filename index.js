a23840664a7b4e584c53532def240ee220f4247b
Rizbi ahmmad <contact.rizbi123@gmail.com>
2025-06-22 13:55:24 +0600

    Adding delete functionality

--- index.js
+++ index.js
@@ -66,6 +66,14 @@ async function run() {
       res.send(result);
     });
 
+        // DELETE endpoint to remove a user
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
